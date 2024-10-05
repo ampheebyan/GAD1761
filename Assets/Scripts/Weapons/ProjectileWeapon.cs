371b539ac5410696e97153ce6bd7c9f8a4c2516e
phoebe <1033478@student.sae.edu.au>
2024-10-05 23:28:39 +1000

    Normalize weapon position and rotation so it actually fires consistently.

--- Assets/Scripts/Weapons/ProjectileWeapon.cs
+++ Assets/Scripts/Weapons/ProjectileWeapon.cs
@@ -17,6 +17,8 @@ public class ProjectileWeapon : BaseWeapon
             if(ammo.x == 0) return;
             ammo.x = Mathf.Clamp(ammo.x - 1, 0, ammo.y);
             GameObject temporaryProjectile = (GameObject) Instantiate(projectile, tip.position, tip.rotation);
+            temporaryProjectile.transform.position.Normalize();
+            temporaryProjectile.transform.rotation.Normalize();
             temporaryProjectile.SetActive(true);
             if(temporaryProjectile.TryGetComponent<BaseProjectileObject>(out BaseProjectileObject obj))
                 obj.SetBase(this);
